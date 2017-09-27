9f0fdc2a2a14bc8c3f9361008d4b6d268df231ab
Jacob Waller <jacobwaller98@gmail.com>
2017-09-26 23:13:39 -0500

    Removed files and updated assignment

--- CS-341 Hummel/Project 2/main.cpp
+++ CS-341 Hummel/Project 2/main.cpp
@@ -1,3 +1,8 @@
+/*
+  Jacob Waler
+  673978936
+  9/25/2017
+*/
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -8,7 +13,7 @@
 #include <numeric>
 #include <functional>
 
-#include "student.h"
+#include "session.h"
 
 using namespace std;
 
@@ -26,7 +31,28 @@ string getArgument(string line, string tag) {
 
 Session getSessionFromFile(string filename) {
   ifstream inputFile(filename);
-  
+  Session currSession;
+  string currAnswer = "";
+  string line;
+  while(getline(inputFile,line)) {
+    while(isspace(line.front())) {
+      line.erase(0,1);
+    }
+    if(line.substr(1,1) == "s") { //Looking at session
+      currSession.setName(getArgument(line,"ssnn"));
+    } else if(line.substr(1,1) == "v") { //Looking at student
+      string studentsAnswer = getArgument(line,"ans");
+      if(studentsAnswer.size() != 0)
+        currSession.studentAnswered(getArgument(line, "id"), currAnswer.find(studentsAnswer) != string::npos);
+    } else if(line.substr(1,1) == "p") { //Looking at a poll
+      currAnswer = getArgument(line,"cans");
+      currSession.newQuestion();
+    } else if(line.substr(1,1) == "/") {
+      //Looking at the end of a block
+      //Don't do anything...
+    }
+  }
+  return currSession;
 }
 
 
@@ -36,9 +62,58 @@ int main() {
   vector<Session> allSessions;
   ifstream inputFile("files.txt");
   string currLine;
+  int numSessions = 0;
+  int numQuestions = 0;
   while(getline(inputFile,currLine)) {
+    cout << ">>Parsing '" << currLine << "'..." << endl;
+    numSessions++;
     allSessions.push_back(getSessionFromFile(currLine));
+    numQuestions += allSessions[allSessions.size()-1].getNumQuestions();
+  }
+
+  cout << endl << "**Class Analysis**" << endl;
+  cout << ">>Total sessions: " << numSessions << endl;
+  cout << ">>Total questions: " << numQuestions << endl;
+
+
+  cout << ">>Answered:" << endl;
+  for(auto& s : allSessions) {
+    cout << "  \"" << s.getName() << "\": " << 100.0*(s.getNumAnswers() /((float)s.getNumQuestions() * s.getNumClickers())) << "% (";
+    cout << s.getNumQuestions() << " questions, " << s.getNumClickers();
+    cout << " clickers, " << s.getNumAnswers() << " answers)" << endl;
+  }
+
+  cout << ">>Correctly:" << endl;
+  for(auto& s : allSessions) {
+    cout << "  \"" << s.getName() << "\": " << 100.0*(s.getNumCorrect() /((double)s.getNumQuestions() * s.getNumClickers())) << "% (";
+    cout << s.getNumQuestions() << " questions, " << s.getNumClickers();
+    cout << " clickers, " << s.getNumCorrect() << " correct answers)" << endl;
+  }
+
+  cout << endl << "**Student Analysis**" << endl;
+
+  cout << ">> Enter a clicker id (# to quit): ";
+  cin >> currLine;
+  while(currLine != "#") {
+    currLine.insert(0,"#");
+    std::vector<Student> tempV;
+    for(auto& s : allSessions) {
+      std::vector<Student>::iterator it = s.getStudentById(currLine);
+      if(it != s.getEndOfStudents()) {
+        Student tmp = *it;
+        tempV.push_back(tmp);
+        cout << "  \"" << s.getName() << "\": " << tmp.getAnswered() << " out of " << s.getNumQuestions();
+        cout << " answered, " << 100.0*(tmp.getCorrect() / (double) s.getNumQuestions()) << "% correctly"<< endl;
+      }
+    }
+    if(tempV.size() == 0) {
+      cout << "**not found..." << endl;
+    }
+
+    cout << endl << ">> Enter a clicker id (# to quit): ";
+    cin >> currLine;
   }
 
 
+  cout << endl << endl <<"**END**";
 }
--- CS-341 Hummel/Project 2/session.h
+++ CS-341 Hummel/Project 2/session.h
@@ -1,39 +1,99 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <algorithm>
+#include "student.h"
+
+using namespace std;
 
 class Session {
 private:
   std::string sessionName;
-  int numQuestions;
+  int numAnswers;
   int numCorrect;
   int numClickers;
+  int numQuestions;
+  int numWrong;
+  std::vector<Student> v;
 public:
-  Session(std::string name)
-  : sessionName(name) {
-    numAnswered = 0;
+  Session() {
     numCorrect = 0;
+    numAnswers = 0;
     numClickers = 0;
+    numQuestions = 0;
+    numWrong = 0;
   }
 
-  void addClicker() {
-    numClickers++;
+  void setName(std::string name) {
+    sessionName = name;
   }
 
-  int getCorrect() {
+  string getName() {
+    return sessionName;
+  }
+
+  int getNumClickers() {
+    return numClickers;
+  }
+
+  int getNumCorrect() {
     return numCorrect;
   }
 
-  void gotCorrect() {
-    numCorrect++;
+  int getNumAnswers() {
+    return numCorrect+numWrong;
   }
 
-  int getNumAsked() {
+  int getNumQuestions() {
     return numQuestions;
   }
 
-  void askedQuestion() {
+  void newQuestion() {
     numQuestions++;
   }
 
-}
+  std::vector<Student>::iterator getEndOfStudents() {
+    return v.end();
+  }
+
+  std::vector<Student>::iterator getStudentById(string id) {
+    std::vector<Student>::iterator it = find_if(v.begin(),v.end(),
+    [id](Student s){
+      return s.getId() == id;
+    });
+    return it;
+  }
+
+  void studentAnswered(std::string id, bool gotRight) {
+    numAnswers++;
+    std::vector<Student>::iterator it;
+
+    it = find_if(v.begin(),v.end(),
+    [&](Student s){
+      return s.getId() == id;
+    });
+
+    if(it == v.end()) {//Student is not in vector
+      Student currStudent(id);
+      if(gotRight) {
+        numCorrect++;
+        currStudent.gotOneRight();
+      } else {
+        numWrong++;
+        currStudent.gotOneWrong();
+      }
+      v.push_back(currStudent);
+      numClickers++;
+    } else {
+      if(gotRight) {
+        numCorrect++;
+        it->gotOneRight();
+      } else {
+        numWrong++;
+        it->gotOneWrong();
+      }
+    }
+  }
+
+};
